0139da176efe3466b7db626284ce962d6c1f9ac1
Angga diki Saputra <anggadikisaputraagung@gmail.com>
2023-12-27 20:43:58 +0700

    create review you bag component in checkout page

--- src/components/templates/CheckoutItems/index.tsx
+++ src/components/templates/CheckoutItems/index.tsx
@@ -1,3 +1,4 @@
+import ItemBag from "@/components/molecules/ItemBag";
 import PaymentCard from "@/components/organismes/PaymentCard";
 import ShippingCard from "@/components/organismes/ShippingCard";
 
@@ -6,6 +7,14 @@ const CheckoutItem = () => {
     <div className=" w-full flex flex-col py-10 gap-6">
       <ShippingCard />
       <PaymentCard />
+      <div className=" bg-white rounded-xl p-4">
+        <p className=" font-cabin text-3xl uppercase tracking-[0.2em]">
+          Review You Bag
+        </p>
+        <ItemBag src="/product.png" title="Apple Watch" />
+        <hr className=" border border-[#1A1F1680]" />
+        <ItemBag src="/product-2.png" title="Dell XPS 13" />
+      </div>
     </div>
   );
 };
